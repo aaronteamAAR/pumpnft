1ce86ec3f34e12348e2317a56c6affd62e50d292
Aaron Chris <rooneychris83@gmail.com>
2022-05-15 23:32:20 +0100

    pulling

--- next.config.js
+++ next.config.js
@@ -8,12 +8,10 @@ const nextConfig = {
 }
 
 
-module.exports = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+export const typescript = {
+  // !! WARN !!
+  // Dangerously allow production builds to successfully complete even if
+  // your project has type errors.
+  // !! WARN !!
+  ignoreBuildErrors: true,
 }
